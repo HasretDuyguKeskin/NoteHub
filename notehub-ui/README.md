899d9ad805827e6c6a51b308b76b48b02168032d
hasret keskin <hasret_1989_@hotmail.com>
2021-05-28 13:09:14 +0300

    login home

--- notehub-ui/README.md
+++ notehub-ui/README.md
@@ -72,5 +72,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 ### links
 
 https://react-bootstrap.github.io/getting-started/introduction
+https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
 
-https://react-bootstrap.github.io/components/forms/
+### vscode extensions
+ES7 React/Redux/GraphQL/React-Native snippets
+
+
--- notehub-ui/src/App.js
+++ notehub-ui/src/App.js
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-// import Button from 'react-bootstrap/Button';
-import Register from './Register'
+// import Register from './Register'
+// import Login from './Login'
+ import Home from './Home'
 
 function App() {
   return (
     <div>
-      {/* <Button variant="danger">Benim Butonum</Button> */}
-      <Register />
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
